4672e09af8b7ad27c80c06c2e7d62a77fe4b1df9
Khatri-Jinal <55817526+Khatri-Jinal@users.noreply.github.com>
2020-08-17 17:32:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function slideshow() {
+  var x = document.getElementById("checking");
+  // if (x.style.display === none) {
+  x.style.display = "block";
+  // } else {
+  // x.style.display = "none";
+  // }
+}
+// feedback`
+function chngbc(black) {
+  document.getElementById("happy").style.background = black;
+}
+/////
+function chngylw() {
+  document.getElementById("1").style.color = "yellow";
+  document.getElementById("2").style.color = "white";
+  document.getElementById("3").style.color = "white";
+  document.getElementById("4").style.color = "white";
+  document.getElementById("5").style.color = "white";
+}
+function chngylw1() {
+  document.getElementById("1").style.color = "yellow";
+  document.getElementById("2").style.color = "yellow";
+  document.getElementById("3").style.color = "white";
+  document.getElementById("4").style.color = "white";
+  document.getElementById("5").style.color = "white";
+}
+function chngylw2() {
+  document.getElementById("1").style.color = "yellow";
+  document.getElementById("2").style.color = "yellow";
+  document.getElementById("3").style.color = "yellow";
+  document.getElementById("4").style.color = "white";
+  document.getElementById("5").style.color = "white";
+}
+function chngylw3() {
+  document.getElementById("1").style.color = "yellow";
+  document.getElementById("2").style.color = "yellow";
+  document.getElementById("3").style.color = "yellow";
+  document.getElementById("4").style.color = "yellow";
+  document.getElementById("5").style.color = "white";
+}
+function chngylw4() {
+  document.getElementById("1").style.color = "yellow";
+  document.getElementById("2").style.color = "yellow";
+  document.getElementById("3").style.color = "yellow";
+  document.getElementById("4").style.color = "yellow";
+  document.getElementById("5").style.color = "yellow";
+}
